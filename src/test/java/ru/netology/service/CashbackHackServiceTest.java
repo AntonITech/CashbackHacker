1839e5cc4bdde5642022cf35a0dc66506d8e147f
AntonI <avmiass@bk.ru>
2023-03-01 22:32:16 +0300

    junit4 added

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,69 @@
+package ru.netology.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+public class CashbackHackServiceTest {
+
+    CashbackHackService service = new CashbackHackService();
+
+    @Test
+    public void testWhenPurchaseEqualThousand() {
+        int amount = 1000;
+
+        int expected = 1000;
+        int actual = service.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testWhenPurchaseAboveThousand() {
+        int amount = 2000;
+
+        int expected = 1000;
+        int actual = service.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testWhenPurchaseAboveThousandOnHundred() {
+        int amount = 1100;
+
+        int expected = 900;
+        int actual = service.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testWhenPurchaseBelowThousand() {
+        int amount = 900;
+
+        int expected = 100;
+        int actual = service.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testWhenPurchaseBelow() {
+        int amount = 500;
+
+        int expected = 500;
+        int actual = service.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testWhenPurchaseEqualZero() {
+        int amount = 0;
+
+        int expected = 1000;
+        int actual = service.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+}
